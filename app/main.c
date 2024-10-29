c11448f7e2ac1bcb921d19304560e9b5e4ec40ab
Hugh Davenport <hugh@davenport.co.nz>
2024-10-29 19:06:23 +1300

    add parse subcommand to split url

--- app/main.c
+++ app/main.c
@@ -8,6 +8,10 @@
 #include <errno.h>
 #include <ctype.h>
 
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
 #define SHA1_IMPLEMENTATION
 #include "sha1.h"
 
@@ -351,6 +355,242 @@ end:
     return ret;
 }
 
+int is_uri_print(char c) {
+    if (!isprint(c)) return 0;
+    // RFC 3986 section 2.2 Reserved Characters
+    switch (c) {
+        case '!':
+        case '#':
+        case '$':
+        case '&':
+        case '\'':
+        case '(':
+        case ')':
+        case '*':
+        case '+':
+        case ',':
+        case '/':
+        case ':':
+        case ';':
+        case '=':
+        case '?':
+        case '@':
+        case '[':
+        case ']':
+            return 0;
+    }
+    return 1;
+}
+
+typedef struct {
+    char *schema;
+    char *user;
+    char *pass;
+    char *host;
+    int port;
+    char *path;
+    char *query;
+    char *anchor;
+} URL;
+
+URL parse_url(char *start, char *end) {
+    URL ret = {0};
+
+    // schema :// user : pass @ host : port / path ? query # anchor
+
+    char *p = start;
+    if (end == NULL) end = p + strlen(p);
+
+    ret.schema = p;
+    while (p && p < end && *p != ':') p ++;
+    *p = 0;
+    if (strcmp(ret.schema, "http") != 0) {
+        fprintf(stderr, "Invalid schema: %s\n", ret.schema);
+    }
+    if (p + 2 >= end || *(p + 1) != '/' || *(p + 2) != '/') {
+        fprintf(stderr, "Invalid URL. Expected ://\n");
+    }
+    p += 3;
+
+    // FIXME: Support IPV6 literal address
+    if (p < end) ret.user = ret.host = p;
+loop:
+    while (p < end) {
+        switch (*p) {
+            case ':': {
+                for (char *tmp = p + 1; tmp < end; tmp ++) {
+                    if (*tmp == '/') {
+                        *tmp = 0;
+                        if (tmp + 1 < end) ret.path = tmp + 1;
+                        break;
+                    } else if (!isdigit(*tmp)) {
+                        ret.port = 0;
+                        break;
+                    } else {
+                        ret.port = 10 * ret.port + (*tmp - '0');
+                    }
+                }
+                if (ret.port > 0) {
+                    *p = 0;
+                    p = ret.path; // Either NULL (eof found above, or set to char after /)
+                } else {
+                    *p = 0;
+                    ret.host = NULL;
+                    if (p + 1 < end) ret.pass = p + 1;
+                    p += 2;
+                }
+            }; break;
+
+            case '@':
+                *p = 0;
+                if (p + 1 < end) ret.host = p + 1;
+                p += 2;
+                break;
+
+            case '/':
+                *p = 0;
+                if (p + 1 < end) ret.path = p + 1;
+                break;
+
+            case '?':
+                *p = 0;
+                if (p + 1 < end) ret.query = p + 1;
+                break;
+
+            case '#':
+                *p = 0;
+                if (p + 1 < end) ret.anchor = p + 1;
+                break;
+
+            default:
+                p++;
+        }
+    }
+
+    if (ret.host == ret.user) ret.user = NULL;
+
+    if (ret.host == NULL) {
+        fprintf(stderr, "Invalid URL. Could not find hostname\n");
+    }
+
+    if (ret.port == 0) {
+        if (strcmp(ret.schema, "http") == 0) ret.port = 80;
+        else fprintf(stderr, "Invalid URL. Could not find port\n");
+    }
+
+    return ret;
+}
+
+
+int peers_file(const char *fname) {
+    FILE *f = fopen(fname, "rb");
+    if (f == NULL) return EX_NOINPUT;
+
+    if (fseek(f, 0, SEEK_END) != 0) goto end;
+    long fsize = ftell(f);
+    if (fsize < 0) goto end;
+    if (fseek(f, 0, SEEK_SET) != 0) goto end;
+
+    char *data = (char *)malloc(fsize);
+    size_t read_total = 0;
+    while (read_total < fsize) {
+        size_t read_count = fread(data, 1, fsize, f);
+        if (read_count == 0) goto end;
+        read_total += read_count;
+    }
+
+    int ret = EX_DATAERR;
+    Value *decoded = decode_bencode(data);
+    if (!decoded) goto end;
+    if (decoded->type != DICT) goto end;
+    Dict *dict = (Dict *)decoded->data;
+    Value *info = dict_value(dict, "info");
+    if (!info) goto end;
+    if (info->type != DICT) goto end;
+    Value *length = dict_value((Dict *)info->data, "length");
+    if (!length || length->type != INTEGER) goto end;
+
+    uint8_t info_hash[SHA1_DIGEST_BYTE_LENGTH];
+    if (!sha1_digest((const uint8_t*)info->start,
+                    (info->end - info->start),
+                    info_hash)) {;
+        goto end;
+    }
+
+    Value *announce = dict_value(dict, "announce");
+    if (!announce) goto end;
+    if (announce->type != BYTES) goto end;
+
+    print_value(announce, (PrintConfig) {.noquotes = true, .newline=true});
+    URL url = parse_url((char *)announce->start, (char *)announce->end);
+
+    printf("schema = %s\n", url.schema);
+    printf("host = %s\n", url.host);
+    printf("port = %d\n", url.port);
+    printf("path = %s\n", url.path);
+    printf("query = %s\n", url.query);
+    printf("anchor = %s\n", url.anchor);
+
+    /* struct addrinfo hints; */
+    /* struct addrinfo *result, *rp; */
+    /* int sfd, s; */
+    /* size_t len; */
+    /* ssize_t nread; */
+    /* char buf[BUF_SIZE]; */
+
+    /* memset(&hints, 0, sizeof(hints)); */
+    /* hints.ai_family = AF_UNSPEC;    /1* Allow IPv4 or IPv6 *1/ */
+    /* hints.ai_socktype = SOCK_DGRAM; /1* Datagram socket *1/ */
+    /* hints.ai_flags = 0; */
+    /* hints.ai_protocol = 0;          /1* Any protocol *1/ */
+
+    /* ret = getaddrinfo(host, port, &hints, &result); */
+    /* if (ret != 0) { */
+    /*     goto end; */
+    /* } */
+
+    /* int sockfd; */
+    /* sockfd = socket(AF_INET, SOCK_STREAM, 0); */
+    /* if (sockfd == -1) { */
+    /*     goto end; */
+    /* } */
+    /* struct sockaddr_in servaddr; */
+    /* bzero(&servaddr, sizeof(servaddr)); */
+    /* servaddr.sin_family = AF_INET; */
+    /* servaddr.sin_addr.s_addr = AF_INET; */
+    /* servaddr.sin_port = 80; */
+    /* connect(sockfd, (SA*)&servaddr, sizeof(servaddr)) */
+
+    printf("info_hash=");
+    for (int idx = 0; idx < SHA1_DIGEST_BYTE_LENGTH; idx ++) {
+        if (!is_uri_print(info_hash[idx])) {
+            printf("%%%02x", info_hash[idx]);
+        } else {
+            printf("%c", info_hash[idx]);
+        }
+    }
+    const char *peer_id = "AdtLtU86udGzzN5m9GDsBCaGAAQ";
+    printf("&peer_id=%s", peer_id);
+    printf("&port=%d", 6881);
+    printf("&uploaded=%d", 0);
+    printf("&downloaded=%d", 0);
+    printf("&left=");
+    print_value(length, (PrintConfig) {.newline = false});
+    printf("&compact=1");
+    printf("\n");
+
+
+end:
+    if (decoded) free_value(decoded);
+    if (errno) {
+        int ret = errno;
+        if (f) fclose(f);
+        return ret;
+    }
+    if (f) if (!fclose(f)) return errno;
+    return ret;
+}
+
 int hash_file(const char *fname) {
     FILE *f = fopen(fname, "rb");
     if (f == NULL) return EX_NOINPUT;
@@ -411,6 +651,18 @@ int main(int argc, char* argv[]) {
     } else if (strcmp(command, "info") == 0) {
         const char* fname = argv[2];
         return info_file(fname);
+    } else if (strcmp(command, "peers") == 0) {
+        const char* fname = argv[2];
+        return peers_file(fname);
+    } else if (strcmp(command, "parse") == 0) {
+        URL url = parse_url(argv[2], NULL);
+        printf("schema = %s\n", url.schema);
+        printf("host = %s\n", url.host);
+        printf("port = %d\n", url.port);
+        printf("path = %s\n", url.path);
+        printf("query = %s\n", url.query);
+        printf("anchor = %s\n", url.anchor);
+        return EX_OK;
     } else if (strcmp(command, "hash") == 0) {
         const char* fname = argv[2];
         return hash_file(fname);
