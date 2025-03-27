9c6cd166608e5ea05034295d0d8512e59feabdf3
Hugh Davenport <hugh@davenport.net.nz>
2025-03-27 19:07:05 +1300

    Get it compiling on codecrafters servers

--- your_bittorrent.sh
+++ your_bittorrent.sh
@@ -8,10 +8,20 @@
 set -e
 tmpFile=$(mktemp)
 cc="${CC:-}"
-for compiler in tcc gcc clang; do
+[ -z "$cc" ] && for compiler in tcc gcc clang; do
     [ -z "$cc" ] && cc=$(command -v $compiler) || continue
-    echo "cc = $cc"
     break
 done
-${cc:-cc} -Wall -Wextra -Wpedantic -Werror -Wno-gnu-zero-variadic-macro-arguments -fsanitize=address -ggdb -lcurl -lcrypto app/*.c -o "$tmpFile"
+echo "cc = ${cc:-cc}" >&2
+CFLAGS="-Wall -Wextra -Wpedantic -Werror"
+CFLAGS="$CFLAGS -ggdb"
+CFLAGS="$CFLAGS -lcurl -lcrypto"
+CFLAGS="$CFLAGS -Wno-gnu-zero-variadic-macro-arguments"
+tmpdir=$(mktemp -d)
+echo "int main(void) { return 0; }" > "$tmpdir/test.c"
+${cc:-cc} -fsanitize=address "$tmpdir/test.c" -o "$tmpFile" 2>/dev/null && CFLAGS="${CFLAGS:-} -fsanitize=address"
+rm "$tmpdir/test.c"
+rmdir "$tmpdir"
+echo "cflags = $CFLAGS" >&2
+${cc:-cc} $CFLAGS app/*.c -o "$tmpFile"
 exec "$tmpFile" "$@"
